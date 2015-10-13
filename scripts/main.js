f192fc850a7381e0db165e79136e409eb0f3e9e3
Dmitrii Maslov <dmitry.aleksandrovich.maslov@gmail.com>
2015-10-14 02:33:37 +0300

    Initial release, guys!!!1111

---
+++ scripts/main.js
@@ -0,0 +1,73 @@
+(function () {
+
+    function loadScript(url, callback) {
+
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+
+        if (script.readyState) { //IE
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" || script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else { //Others
+            script.onload = function () {
+                callback();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+
+    loadScript("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", function () {
+        
+// After jQuery loaded
+
+        ripKirillWebms();
+
+    });
+
+
+})();
+
+function ripKirillWebms()
+{
+
+        var kirillPosts = $('[href="/assa100500"]').closest('.im_in:not(.ripped)');
+        
+        var regex = new RegExp(".webm");
+
+        kirillPosts = kirillPosts.filter(function () {
+            return regex.test($(this).find('.im_msg_text').text()); 
+        });
+        
+        kirillPosts.each(function(){
+            
+            var kirillPost = $(this);
+            
+            var placeholder = $('<tr class="im_in" data-from="" data-date="">\n\
+  <td class="im_log_act">\n\
+    <div id="" class="im_log_check_wrap"><div class="im_log_check" id="mess_check81441"></div></div>\n\
+  </td>\n\
+  <td class="im_log_author"><div class="im_log_author_chat_thumb"><span><img src="https://pp.vk.me/c308316/v308316100/b2b6/HzaouMMQQXo.jpg" class="im_log_author_chat_thumb" width="32" height="32"></span></div></td>\n\
+  <td class="im_log_body"><div class="wrapped"><div class="im_log_author_chat_name"><span class="mem_link" target="_blank">Проклятый Бомж webm-ripper</span></div><div class="im_msg_text">Проклятый Бомж\'s webm was successfully pwned. <a class="showwebm">Показать</a></div></div></td>\n\
+  <td class="im_log_date"><a class="im_important_toggler" onclick="return IM.toggleImportant(81441);" onmouseover="IM.showImportantTT(this);"></a><input type="hidden" id="im_date81441" value="1444730283"></td>\n\
+  <td class="im_log_rspacer"></td>\n\
+</tr>');
+            
+            placeholder.find('a.showwebm').click(function(){
+                
+            placeholder.hide();
+            kirillPost.show();
+            
+            });
+            
+            $(this).addClass('ripped').hide().after(placeholder);
+        
+            
+        });
+    
+}
